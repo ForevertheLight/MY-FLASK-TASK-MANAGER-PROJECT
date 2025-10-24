024705e4de3502036f645cf22ad06b17e52cb647
ForevertheLight <fashjosh2004@gmail.com>
2025-10-24 14:06:39 +0100

    Commit to validate payload - ensure it exists, is JSON(Dictionary), and not empty

---
+++ utils/validators.py
@@ -0,0 +1,18 @@
+import re
+#'re' stands for regular expressions 
+#It allows you to 'search','match','validate',or'replace' text patterns inside strings.
+#For example,checking if a name only contains letters, 
+# or if a password meets certain complexity rules.
+
+# Payload validation
+def validate_payload(payload):
+    #Checks if payload is supplied by the user
+    if payload is None:
+        return "Payload is missing"
+    #Checks if payload is a Dictionary datatype
+    if not isinstance(payload, dict):
+        return "Payload must be a valid JSON object"
+    #Checks if payload is not empty
+    if not payload:
+        return "Payload cannot be empty"
+    return None
